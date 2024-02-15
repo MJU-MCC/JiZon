825f49c898a96f5bfc028817b9a3a0ce06fce0bf
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-02-15 16:24:51 +0900

    여자구현체 생성

--- JavaSt/src/OneWeek/WomanGuest.java
+++ JavaSt/src/OneWeek/WomanGuest.java
@@ -1,26 +1,26 @@
 package OneWeek;
 
-public class ManGuest implements Guest{
+public class WomanGuest implements Guest{
 
     private String name ;
     @Override
     public void greeting() {
-        System.out.println("안녕하세요 저의 이름은 " + this.name + "입니다.");
+        System.out.println("안녕하세요 여자인 저의 이름은 " + this.name + "입니다.");
     }
 
     @Override
     public String order(String menu,int number) {
-        System.out.println(menu + "로 "+number+"잔 주세요");
+        System.out.println(menu + "로 "+number+"개 주세요");
         return menu;
     }
 
 
     @Override
     public void pickUpDrink(String Drink) {
-        System.out.println(Drink + "잘 마시겠습니다. 감사합니다.");
+        System.out.println(Drink + "알아서 마시겠습니다. 감사합니다.");
     }
 
-    public ManGuest(String name) {
+    public WomanGuest(String name) {
         this.name = name;
     }
 }
