cfac87b509f7d182254c183f7bef0c99334bd1a9
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-02-20 18:12:16 +0900

    인영의 포인트 적립 기능 생성

--- JavaSt/src/TwoWeek/Inyoung/Calculator.java
+++ JavaSt/src/TwoWeek/Inyoung/Calculator.java
@@ -1,4 +1,25 @@
 package TwoWeek.Inyoung;
 
 public class Calculator {
+    public void setSt(Setting st) {
+        this.st = st;
+    }
+
+    private Setting st;
+    private int sumMoney;
+
+    public int calc(){
+        int result = st.price * st.number;
+        sumMoney += result;
+        return result;
+    }
+
+    public void sumInfo(){
+        System.out.println("손님이 주문하신 음료의 총 금액은 " + sumMoney);
+    }
+
+    public void savePoint(){
+        int point = sumMoney / 10;
+        System.out.println("주문하신 금액은 " + sumMoney + " 이고 적립되는 포인트는 " + point + "입니다.");
+    }
 }
--- JavaSt/src/TwoWeek/Inyoung/Inyoung.java
+++ JavaSt/src/TwoWeek/Inyoung/Inyoung.java
@@ -17,8 +17,19 @@ public class Inyoung {
         System.out.println(siren.priceMent());
         System.out.println(siren.order());
 
-        Customer me = new Customer("박인영", 5000); //여기 포인트 적립부분을 Calculator 클래스를 활용해서 수식 계산후 기입하고 싶은데 어떻게 해야할지 모르겠음...
-        System.out.println(me.showInfo());
+        Calculator calculator = new Calculator();
+        calculator.setSt(takeout);
+        calculator.calc();
+        calculator.setSt(smartOrder);
+        calculator.calc();
+        calculator.setSt(siren);
+        calculator.calc();
+
+        calculator.sumInfo();
+        calculator.savePoint();
+
+//        Customer me = new Customer("박인영", 5000); //여기 포인트 적립부분을 Calculator 클래스를 활용해서 수식 계산후 기입하고 싶은데 어떻게 해야할지 모르겠음...
+//        System.out.println(me.showInfo());
 
     }
 }
