9c5f638f56c7c36e94a1969984c02dd4fb7764ea
yhs37 <dbsgustjs730@naver.com>
2024-02-28 18:09:43 +0900

    현선 : 백준 10810 수정

--- JavaSt/src/ThreeWeek/HyunSeon/BK_10810.java
+++ JavaSt/src/ThreeWeek/HyunSeon/BK_10810.java
@@ -1,2 +1,24 @@
-package ThreeWeek.HyunSeon;public class BK_10810 {
+package ThreeWeek.HyunSeon;
+
+import java.util.Scanner;
+
+public class BK_10810 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bas = sc.nextInt();
+        int num = sc.nextInt();
+        int number[] = new int[bas];
+
+        for (int i = 0; i < num; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            for (int j = a; j <= b; j++) {
+                number[j - 1] = c;
+            }
+        }
+        for (int i = 0; i < bas; i++) {
+            System.out.println(number[i]);
+        }
+    }
 }
