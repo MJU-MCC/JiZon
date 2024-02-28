165bd7a40f31e10c79e15cb78e9bccb5e007717d
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-02-28 19:40:12 +0900

    [현성] : 브랜치 테스트2

--- JavaSt/src/ThreeWeek/HyunSeong/gitPrac/GitP.java
+++ JavaSt/src/ThreeWeek/HyunSeong/gitPrac/GitP.java
@@ -3,6 +3,7 @@ package ThreeWeek.HyunSeong.gitPrac;
 public class GitP {
     public static void main(String[] args) {
         System.out.println("Git");
+        System.out.println("Git1");
 
     }
 }
