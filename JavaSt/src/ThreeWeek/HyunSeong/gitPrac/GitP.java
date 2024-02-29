86ae980d955d9358be23d9e3fbbe01670c17966a
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-03-01 00:42:54 +0900

    [현성] : 자바 컴파일하고 터미널에서 자바 실행하는 방법

--- JavaSt/src/ThreeWeek/HyunSeong/gitPrac/GitP.java
+++ JavaSt/src/ThreeWeek/HyunSeong/gitPrac/GitP.java
@@ -9,10 +9,15 @@ public class GitP {
 //        System.out.println("Git4");
 //        System.out.println("Git6");
 
-        char ch = 'A'; // 문자 'A'를 선언
-        int unicodeValue = (int) ch; // 문자형을 정수로 변환하여 유니코드 값 저장
-        System.out.println("유니코드 값: " + ch); // 결과 출력
-        System.out.println("유니코드 값: " + unicodeValue); // 결과 출력
+//        char ch = 'A'; // 문자 'A'를 선언
+//        int unicodeValue = ch; // 문자형을 정수로 변환하여 유니코드 값 저장
+//        System.out.println("유니코드 값: " + ch); // 결과 출력
+//        System.out.println("유니코드 값: " + unicodeValue); // 결과 출력
+
+        short shortValue = 1000; // short 타입 변수 선언 및 값 할당
+        // short 값을 byte 타입으로 변환하여 변수에 저장
+        byte byteValue = (byte) shortValue;
+        System.out.println("변환된 값: " + byteValue); // 결과 출력
 
     }
 }
