11321f6e35967e2ee9d7975a103402b3b2f3408b
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-03-06 13:22:14 +0900

    [현성] : 백준 11654번 문제풀이

---
+++ JavaSt/src/ThreeWeek/HyunSeong/Baekjoon/B11654.java
@@ -0,0 +1,13 @@
+package ThreeWeek.HyunSeong.Baekjoon;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+public class B11654 {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println();
+    }
+}
