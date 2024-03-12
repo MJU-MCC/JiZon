b9d0baaadd3f43bb8c74f06b8e2cee15efb3cd13
JsHaaaaaan <162900180+JsHaaaaaan@users.noreply.github.com>
2024-03-12 18:52:16 +0900

    Add files via upload

---
+++ JavaSt/src/TwoWeek/JongSeok/Constant.java
@@ -0,0 +1,12 @@
+package Week2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.14;
+		String lit = "문자나 숫자는 리터럴"; 
+		System.out.println(PI + lit);
+	}
+
+}
---
+++ JavaSt/src/TwoWeek/JongSeok/Type.java
@@ -0,0 +1,13 @@
+package Week2;
+
+public class Type {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "안녕하세요";
+		int b = 1212;
+		char c = '뇽';
+		System.out.print(a + b + c);
+	}
+
+}
