bbb491b6721165ed34f199f61d682cec0a20fb1e
JOO <zpffjtm@naver.com>
2024-03-14 20:48:16 +0900

    OverfloweExample

--- JavaSt/src/TwoWeek/Jooyeon/Calculation_Of_PlusMinus.java
+++ JavaSt/src/TwoWeek/Jooyeon/Calculation_Of_PlusMinus.java
@@ -8,6 +8,16 @@ public class Calculation_Of_PlusMinus {
         System.out.println();
         System.out.println(1+3);
         System.out.println(5-3);
+        System.out.println("a" + "b");
+        System.out.println('a' + 'b'); // 아스키코드 형태로 나오게 된다.
+        int x = 'a';
+        System.out.println(x);
+
+        char ch =97;
+        System.out.println(ch);
+
+        char cha = 65003;
+        System.out.println(cha);
         System.out.println("----------------------------------------------------");
 
     }
