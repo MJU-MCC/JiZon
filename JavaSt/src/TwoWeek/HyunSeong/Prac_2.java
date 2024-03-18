9c4880f8d322d84299369f57a63f7c406b43e26b
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-03-18 14:50:21 +0900

    [현성] : 스터디 hs 브랜치 커밋 테스트

--- JavaSt/src/TwoWeek/HyunSeong/Prac_2.java
+++ JavaSt/src/TwoWeek/HyunSeong/Prac_2.java
@@ -13,29 +13,29 @@ public class Prac_2 {
 //        System.out.println(2);
 //
 //        //아래 두개의 결과를 예측해보셔요
-////        System.out.println("a" + "b");
-////        System.out.println('a' + 'b');
+//        System.out.println("a" + "b");
+//        System.out.println('a' + 'b');
 //
 //
-//        byte by = 127;
-////        byte by2 = 128;
-//        System.out.println("by = " + by);
+        byte by = 127;
+//        byte by2 = 128;
+        System.out.println("by = " + by);
 //
 //            short shortValue = 1000; // short 타입 변수 선언 및 값 할당
 //            // short 값을 byte 타입으로 변환하여 변수에 저장
 //            byte byteValue = (byte) shortValue;
 //            System.out.println("변환된 값: " + byteValue);
-        float x = 0f;
-        for(int i =0 ; i<10; i++){
-            x+= 0.1;
-        }
-        System.out.println("x = " + x);
-        System.out.println("=======================");
-        double y = 0;
-        for(int i =0 ; i<10; i++){
-            y+= 0.1;
-        }
-        System.out.println("y = " + y);
+//        float x = 0f;
+//        for(int i =0 ; i<10; i++){
+//            x+= 0.1;
+//        }
+//        System.out.println("x = " + x);
+//        System.out.println("=======================");
+//        double y = 0;
+//        for(int i =0 ; i<10; i++){
+//            y+= 0.1;
+//        }
+//        System.out.println("y = " + y);
 
 // 결과 출력
 //변환된 값: -24
