eb190798a2401d1adda8cebc1252af49d7b97ed1
younglee8867 <ljy031112@gmail.com>
2024-03-19 00:50:24 +0900

    3/19 study

---
+++ JavaSt/src/ThreeWeek/Jooyeong/Operators_and_Operands.java
@@ -0,0 +1,14 @@
+package ThreeWeek.Jooyeong;
+
+public class Operators_and_Operands {
+    
+    public static void main(String[] args) {
+        //01. 연산자와 피연산자
+
+        int x = 5, y;
+        y = 4 * x + 3;
+
+        // System.out.println(23);
+        System.out.println(y);
+    }
+}
