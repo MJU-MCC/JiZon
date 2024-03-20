daaf5f991b9557872185d774bbd8b16be21e3825
younglee8867 <ljy031112@gmail.com>
2024-03-20 22:31:57 +0900

    3/20 chapter3

---
+++ JavaSt/src/ThreeWeek/Jooyeong/basic_operations.java
@@ -0,0 +1,10 @@
+package ThreeWeek.Jooyeong;
+
+public class basic_operations {
+    
+    public static void main(String[] args) {
+        
+        float a = 5.3f;
+        System.out.println(a/0);
+    }
+}
