14c9ca4eeb08559fc1091eaf51e7af62888f655d
yhs37 <dbsgustjs730@naver.com>
2024-03-21 21:02:01 +0900

    [현선]:수정

--- JavaSt/src/ThreeWeek/HyunSeon/Switch.java
+++ JavaSt/src/ThreeWeek/HyunSeon/Switch.java
@@ -5,8 +5,9 @@ public class Switch {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
-         switch(score%10) {
-             case 9 :
+         switch(score/10) {
+             case 10 :
+             case 9  :
                  System.out.println("A");
                  break;
              case 8 :
