11e1ecbc7b4bddd27c053f3161c25c7b396ba973
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-04-09 14:08:58 +0900

    [현성] : 백준 10988 세팅

---
+++ JavaSt/src/SixWeek/Hyunseong/BaeC/B10988.java
@@ -0,0 +1,7 @@
+package SixWeek.Hyunseong.BaeC;
+
+public class B10988 {
+    public static void main(String[] args) {
+
+    }
+}
--- JavaSt/src/SixWeek/Hyunseong/kimHyun.java
+++ JavaSt/src/SixWeek/Hyunseong/kimHyun.java
@@ -1,7 +1,13 @@
 package SixWeek.Hyunseong;
+import java.io.*;
 
 public class kimHyun {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException{
+        MyThread th1 = new MyThread();
+        MyThread th2 = new MyThread();
+        Mythread2 th3 = new Mythread2();
+        th1.start();
+        th2.start();
+        th3.start();
     }
 }
