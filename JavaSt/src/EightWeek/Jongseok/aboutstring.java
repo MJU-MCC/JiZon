bbeb4d6549898e7f30d04e46ff4b344cee4ab0e3
HJS82 <gkswhdtjr000802@gmail.com>
2024-05-13 21:30:45 +0900

    1111

--- JavaSt/src/EightWeek/Jongseok/aboutstring.java
+++ JavaSt/src/EightWeek/Jongseok/aboutstring.java
@@ -1,5 +1,7 @@
 package EightWeek.Jongseok;
 
+import java.util.StringTokenizer;
+
 public class aboutstring {
     public static void main(String[] args) {
         String str1 = "hello";
@@ -12,10 +14,15 @@ public class aboutstring {
         System.out.println("str3 주소 :" + Integer.toHexString(System.identityHashCode(str3)));
         System.out.println("str4 주소 :" + Integer.toHexString(System.identityHashCode(str4)));
         System.out.println("str5 주소 :" + Integer.toHexString(System.identityHashCode(str5)));
+        System.out.println();
+
         System.out.println("str1 == str4 " + (str1 == str4));
         System.out.println("str1 == str5 " + (str1 == str5));
+        System.out.println("str1 hasccode :" + str1.hashCode());
+        System.out.println("str5 hasccode :" + str5.hashCode());
         System.out.println("str1.equals(str5) " +str1.equals(str5));
         System.out.println();
+
         System.out.println("str2와 str3의 해시코드 " + (str2.hashCode() == str3.hashCode()));
         System.out.println("str2 == str3 " + (str2 == str3));
         System.out.println("hello world 주소 :" + Integer.toHexString(System.identityHashCode("hello world")));
@@ -24,6 +31,5 @@ public class aboutstring {
         System.out.println("str2.equals(str3) " + str2.equals(str3));
         System.out.println("str1 + \" world\" == str3 " +((str1 + " world") == str3));
         System.out.println("str3.equals(str1 + \" world\") " + str3.equals(str1 + " world"));
-
     }
 }
